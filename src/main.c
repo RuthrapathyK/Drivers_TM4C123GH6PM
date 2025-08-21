20323602886603b86bad56e1bbb818ad3e2c672a
Ruthrapathy-Karuppanasamy_HON <ruthrapathy.karuppanasamy@honeywell.com>
2025-08-21 16:38:36 +0530

    Comparator Working But need to check why Output is not working in Pin

--- src/main.c
+++ src/main.c
@@ -1,14 +1,38 @@
 #include "common.h"
 
-volatile uint8_t counter = 5;
+uint8_t comp_status = 0;
 
 void main()
 {
-  volatile uint8_t counter_2 = 6;
+  /* Enable Clock for Analog Comparator */
+  SYSCTL->RCGCACMP |= 1<<0;
+
+  /* Enable Clock for the GPIO Ports C and F */
+  SYSCTL->RCGCGPIO |= (1<<5 | 1<<2);
+
+  /* Configure the GPIO Pins for Analog Comparator's Input and Output */
+  GPIOC->AFSEL &= ~(1<<6); GPIOC->DIR &= ~(1<<6); GPIOC->ODR &= ~(1<<6); 
+  GPIOC->DEN &= ~(1<<6); GPIOC->PUR &= ~(1<<6); GPIOC->PDR &= ~(1<<6);  // Configure PC6(C0+) as Comaparator Input
+  
+  GPIOC->AFSEL &= ~(1<<7); GPIOC->DIR &= ~(1<<7); GPIOC->ODR &= ~(1<<7); 
+  GPIOC->DEN &= ~(1<<7); GPIOC->PUR &= ~(1<<7); GPIOC->PDR &= ~(1<<7);  // Configure PC7(C0-) as Comparator Input
+  
+  GPIOF->AFSEL |= 1<<0; GPIOF->ODR &= ~(1<<0); GPIOF->DEN |= 1<<0; // Configure PF0(C0o) as Comparator Output
+  GPIOF->PCTL |= 9<<0;
+
+  /* Configure Internal Reference Voltage to 1.65V */
+  COMP->ACREFCTL = 0x30C;
+
+  /* Configure Comparator to use Internal Reference Voltage */
+  COMP->ACCTL0 = 0x40C;
+
+  /* Delay for 10us */
+  for(uint32_t i = 0; i < 1000000; i++)
+  ;
 
   while(1)
   {
-    counter++;
-    counter_2++;
+     /* Read the Comparator Output Value */
+    comp_status = ((COMP->ACSTAT0 >> 1) & 0x01);
   }
 }
