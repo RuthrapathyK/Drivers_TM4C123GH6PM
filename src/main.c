8725c419ffb98b7b15ed8b834800fbef44acd07c
RuthrapathyK <kruthrapathy@gmail.com>
2025-09-13 01:21:42 +0530

    ADC in interrupt mode

--- src/main.c
+++ src/main.c
@@ -3,16 +3,32 @@
 #include "uart.h"
 #include "nvic.h"
 
-volatile uint32_t convData = 0;
+#define SAMPLE_SIZE 2000
+volatile uint32_t convData[SAMPLE_SIZE] = {0};
 
+uint32_t index = 0;
+void ADC0_Sequence_3_handler(void)
+{
+  /* Clear the Interrupt */
+  ADC0->ISC |= 1<<3;
+
+  /* Read the ADC data*/
+  convData[index] = (ADC0->SSFIFO3 & 0x0FFF);
+
+  index++;
+
+  if(index <= SAMPLE_SIZE)
+  {
+    /* Trigger Sampling in SS3*/
+    ADC0->PSSI |= 1<<3;
+  }
+
+}
 void main()
 {
-
   UART_Init(115200);
   Pin_Config(Port_PA, 0, PA0_U0RX);
   Pin_Config(Port_PA, 1, PA1_U0TX);
-  
-  UART_sendString("Hello World\n");
 
   /* Enable ADC0 clock */
   SYSCTL->RCGCADC |= 1<<0;
@@ -29,25 +45,37 @@ void main()
   /* Configure Input Source for the Sample Sequencer - AIN0 */
   ADC0->SSMUX3 &= ~(0xF <<0);
 
+  /* Enable Interrupt of SS3 */
+  ADC0->IM |= 1<<3;
+  ADC0->SSCTL3 |= 1<<2;
+
   /* Configure Sample Control Bits - Set End of Sequence bit */
   ADC0->SSCTL3 |= 1<<1;
 
   /* Enable The Sample Sequencer - SS3*/
   ADC0->ACTSS |= (1<<3);
 
+  /* Enable NVIC Interrupt for ADC0 - SS3 */
+  NVIC_enableInterrupt(ADC_0_SEQ_3_IRQ);
+
+  /* Trigger Sampling in SS3*/
+  ADC0->PSSI |= 1<<3;
+
   while(1)
   {
-    /* Trigger Sampling in SS3*/
-    ADC0->PSSI |= 1<<3;
 
-    /* Wait till ADC conversion is completed */
-    while(((ADC0->ACTSS >> 16) & 0x01))
-    ;
+    if(index > SAMPLE_SIZE)
+    {
+      for(uint32_t iter = 0; iter < SAMPLE_SIZE; iter++)
+      {
+        UART_sendNumber(convData[iter]);
+        UART_sendChar('\n');
+      }
+      index = 0;
 
-    /* Read the ADC data*/
-    convData = (ADC0->SSFIFO3 & 0x0FFF);
+      /* Trigger Sampling in SS3*/
+      ADC0->PSSI |= 1<<3;
+    }
 
-    UART_sendNumber(convData);
-    UART_sendChar('\n');
   }
 }
