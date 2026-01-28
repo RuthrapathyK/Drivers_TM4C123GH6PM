400835c70319cca554c7b0150ab7dd62119c605e
RuthrapathyK <kruthrapathy@gmail.com>
2026-01-28 21:53:48 +0530

    Added Common Test Case

--- test/test_common.c
+++ test/test_common.c
@@ -16,22 +16,38 @@ void tearDown(void)
 {
 }
 
-void test_common_0(void)
+void test_common_RegWrite_Bits_0(void)
 { 
     LARGE_INTEGER frequency, start, end;
     QueryPerformanceFrequency(&frequency); // Get counts per second   
     QueryPerformanceCounter(&start);
 
-    for(int iter = 0; iter < 100000; iter++)
+    for(int iter = 0; iter < 1; iter++)
     {
         RegWrite_Bits(&regVal, 0, 0, 32);
     }
 
     QueryPerformanceCounter(&end);   
     double elapsed_us = (double)(end.QuadPart - start.QuadPart) * 1000000.0 / frequency.QuadPart;   
-    TEST_ASSERT_LESS_THAN_FLOAT(0.0, elapsed_us);
+    TEST_ASSERT_LESS_THAN_FLOAT(500.0, elapsed_us);
+}
 
+void test_common_RegWrite_Bits_1(void)
+{ 
+    RegWrite_Bits(&regVal, 0, 0, 32);
     TEST_ASSERT_EQUAL_UINT32(0x0000000, regVal);
 }
 
+void test_common_delayLoop_0(void)
+{
+    LARGE_INTEGER frequency, start, end;
+    QueryPerformanceFrequency(&frequency); // Get counts per second   
+    QueryPerformanceCounter(&start);
+
+    delayLoop(1000);
+
+    QueryPerformanceCounter(&end);   
+    double elapsed_us = (double)(end.QuadPart - start.QuadPart) * 1000000.0 / frequency.QuadPart;   
+    TEST_ASSERT_LESS_THAN_FLOAT(10000.0, elapsed_us);
+}
 #endif // TEST
