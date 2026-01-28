78bd3ba5c13fd9b9b51a33cfb4b5719f7b8e34fb
RuthrapathyK <kruthrapathy@gmail.com>
2026-01-28 21:16:51 +0530

    Included Timing configuration

--- test/test_common.c
+++ test/test_common.c
@@ -2,6 +2,7 @@
 #ifdef CEEDLING_TEST
 
 #include "unity.h"
+#include <windows.h>
 
 #include "common.h"
 
@@ -16,8 +17,20 @@ void tearDown(void)
 }
 
 void test_common_0(void)
-{   
-    RegWrite_Bits(&regVal, 0, 0, 32);
+{ 
+    LARGE_INTEGER frequency, start, end;
+    QueryPerformanceFrequency(&frequency); // Get counts per second   
+    QueryPerformanceCounter(&start);
+
+    for(int iter = 0; iter < 100000; iter++)
+    {
+        RegWrite_Bits(&regVal, 0, 0, 32);
+    }
+
+    QueryPerformanceCounter(&end);   
+    double elapsed_us = (double)(end.QuadPart - start.QuadPart) * 1000000.0 / frequency.QuadPart;   
+    TEST_ASSERT_LESS_THAN_FLOAT(0.0, elapsed_us);
+
     TEST_ASSERT_EQUAL_UINT32(0x0000000, regVal);
 }
 
