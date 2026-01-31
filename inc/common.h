80b857dfae13f25a243ed89fbd77899f0a17f899
RuthrapathyK <kruthrapathy@gmail.com>
2026-01-31 18:36:09 +0530

    Inlining worded

--- inc/common.h
+++ inc/common.h
@@ -35,8 +35,31 @@
   }
 #endif
 
-void RegWrite_Bits(volatile uint32_t * reg, uint32_t reg_val, uint8_t start_bit, uint8_t bit_length);
-uint32_t RegRead_Bits(volatile uint32_t * reg, uint8_t start_bit, uint8_t bit_length);
+__INLINE__ void RegWrite_Bits(volatile uint32_t * reg, uint32_t reg_val, uint8_t start_bit, uint8_t bit_length)
+{
+    /* Check the Input Parameters */
+    ASSERT((reg != NULL) && (start_bit < 32) && (bit_length > 0) && (bit_length <= 32) && (start_bit + bit_length <= 32) && (reg_val < (1UL << bit_length)));
+    
+    uint32_t temp = *reg;
+
+    /* Clear the Bits to 0 */
+    temp &= ~(((uint32_t)0xFFFFFFFF >> (32 - bit_length)) << start_bit);
+
+    /* Write the Value in specified bits */
+    temp |= reg_val << start_bit;
+
+    /* To avoid Unknown operation while clearing and writing new value, temp variable is used */
+    *reg = temp;
+}
+
+__INLINE__ uint32_t RegRead_Bits(volatile uint32_t * reg, uint8_t start_bit, uint8_t bit_length)
+{
+    /* Check the Input Parameters */    
+    ASSERT((bit_length <= 32) && (start_bit + bit_length <= 32) && (bit_length > 0));
+
+    return ((*reg >> start_bit) & (uint32_t)(((uint64_t)1 << (uint64_t)bit_length) - (uint64_t)1));
+}
+
 void delayLoop(uint32_t mSec);
 
 #endif
