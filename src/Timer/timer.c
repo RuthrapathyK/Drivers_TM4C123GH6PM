1d1b40b970bc62d835845eb6cddfe3a31cf31259
RuthrapathyK <kruthrapathy@gmail.com>
2026-07-05 18:29:58 +0530

    Working at 10us Trigger but not able to achieve 1MSPS

--- src/Timer/timer.c
+++ src/Timer/timer.c
@@ -6,7 +6,7 @@
 void Timer_0A_16_32_handler(void)
 {
     /* Clear Interrupt Status */
-    REG_WRITE(TIMER0->ICR, 1, 0, 1);
+    REG_SET_BIT(TIMER0->ICR, 0);
 }
 
 void TIM_Init(void)
@@ -26,17 +26,18 @@ void TIM_Init(void)
     REG_WRITE(TIMER0->CTL, 0, 0, 1);
 
     /* Write Configurations of Timer Module */
-    REG_WRITE(TIMER0->CFG, 4, 0, 3);  // Select 16-bit Timer without concatenation
-    REG_WRITE(TIMER0->TAMR, 2, 0, 2); // Periodic mode
-    REG_WRITE(TIMER0->TAMR, 0, 4, 1); // Downcount mode
-    
+    REG_WRITE(TIMER0->CFG, TIM_NonConcatenatedMode, 0, 3);  // Select 16-bit Timer without concatenation
+    REG_WRITE(TIMER0->TAMR, TIM_Periodic, 0, 2); // Periodic mode
+    REG_WRITE(TIMER0->TAMR, TIM_CountDown, 4, 1); // Downcount mode
+    REG_WRITE(TIMER0->TAMR, TIM_ReloadSameCycle, 8, 1); //Reload values upon timeout not on next cycle
+
     /* Configure Prescalar */
-    REG_WRITE(TIMER0->TAPR, 16, 0, 8);
+    REG_WRITE(TIMER0->TAPR, 0, 0, 8);
 
     /* Load Start Value to start counting */
-    REG_WRITE(TIMER0->TAILR, 8, 0, 16); // Load the value to generate 10 us Once interrupt
+    REG_WRITE(TIMER0->TAILR, 159, 0, 16); // Load the value to generate 10us us Once interrupt
 
-    /* Configure Interrupt Mask */
+    /* Configure Interrupt Mask - Disable Interrupt */
     REG_WRITE(TIMER0->IMR, 0, 0, 1); // Timeout Interrupt Mask
 
     /* Configure Timer to Trigger ADC conversion */
--- src/Timer/timer.h
+++ src/Timer/timer.h
@@ -3,6 +3,29 @@
 
 #include "common.h"
 
+
+typedef enum{
+    TIM_ConcatenatedMode = 0x0,
+    TIM_RTCMode = 0x1,
+    TIM_NonConcatenatedMode = 0x4
+}TIM_BlockMode;
+
+typedef enum{
+    TIM_OneShot = 1,
+    TIM_Periodic = 2,
+    TIM_Capture = 3
+}TIM_Mode;
+
+typedef enum{
+    TIM_CountDown = 0,
+    TIM_CountUp = 1
+}TIM_Direction_e;
+
+typedef enum{
+    TIM_ReloadSameCycle = 0,
+    TIM_ReloadNextCycle = 1
+}TIM_Reload_e;
+
 void TIM_Init(void);
 
 #endif
